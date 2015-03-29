f9b788e520fd6bec2ce3a8e6e3daba6e61a9b27b
mazhao_ubuntu <mazhao@Aspire-M5-481G>
2015-03-30 00:03:40 +0800

    页面切换动画优化

--- market/src/com/mark/market/ui/MainActivity.java
+++ market/src/com/mark/market/ui/MainActivity.java
@@ -1,12 +1,12 @@
 package com.mark.market.ui;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,28 +15,26 @@ import android.widget.ImageView;
 
 import com.mark.market.R;
 
-public class MainActivity extends FragmentActivity implements OnClickListener{
-
+public class MainActivity extends FragmentActivity implements OnClickListener {
 
 	private Fragment_home fragment_home;
 	private Fragment_my fragment_my;
-	//���岼�ֶ���
-	private FrameLayout layout_home,layout_my;
-	//����ͼƬ�������
-	private ImageView image_home,image_my;
-	//�м���������ť
+	// ���岼�ֶ���
+	private FrameLayout layout_home, layout_my;
+	// ����ͼƬ�������
+	private ImageView image_home, image_my;
+	// �м���������ť
 	private Button tab_plus;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		initView();
 		initData();
-		
-//Ĭ�Ͻ�����ҳ
+
+		// Ĭ�Ͻ�����ҳ
 		clickHomeBtn();
 	}
 
@@ -55,111 +53,106 @@ public class MainActivity extends FragmentActivity implements OnClickListener{
 		return super.onOptionsItemSelected(item);
 	}
 
+	/*
+	 * ��ʼ�����
+	 */
+	private void initView() {
+		// ʵ�������������
+		layout_home = (FrameLayout) findViewById(R.id.layout_home);
+		layout_my = (FrameLayout) findViewById(R.id.layout_my);
 
-/*	
-	��ʼ�����
-	*/
-private void initView(){
-	//ʵ�������������
-	layout_home=(FrameLayout)findViewById(R.id.layout_home);
-	layout_my=(FrameLayout)findViewById(R.id.layout_my);
-	
-	image_home=(ImageView)findViewById(R.id.image_home);
-	image_my=(ImageView)findViewById(R.id.image_my);
-	tab_plus=(Button)findViewById(R.id.tab_plus);
+		image_home = (ImageView) findViewById(R.id.image_home);
+		image_my = (ImageView) findViewById(R.id.image_my);
+		tab_plus = (Button) findViewById(R.id.tab_plus);
 
-	
-}
-/*	
-��ʼ������,������Ҫ��Ϊ�ؼ���Ӽ����¼�
-*/
-
-private void initData(){
-	
-	layout_home.setOnClickListener(this);
-	layout_my.setOnClickListener(this);
-	tab_plus.setOnClickListener(this);
-	
-	
-}
-
-
-@Override
-public void onClick(View v) {
-	// TODO Auto-generated method stub
-	
-	switch(v.getId())
-	{
-	case R.id.layout_home:
-		clickHomeBtn();
-		break;
-	case R.id.layout_my:
-		clickMyBtn();
-		break;
-	case R.id.tab_plus:
-		clickPlusBtn();
-		break;
 	}
-	
-}
 
+	/*
+	 * ��ʼ������,������Ҫ��Ϊ�ؼ���Ӽ����¼�
+	 */
 
-/**
+	private void initData() {
+
+		layout_home.setOnClickListener(this);
+		layout_my.setOnClickListener(this);
+		tab_plus.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+		switch (v.getId()) {
+		case R.id.layout_home:
+			clickHomeBtn();
+			break;
+		case R.id.layout_my:
+			clickMyBtn();
+			break;
+		case R.id.tab_plus:
+			clickPlusBtn();
+			break;
+		}
+
+	}
+
+	/**
  * 
  */
-private void clickPlusBtn() {
-	// TODO Auto-generated method stub
-	//������Ʒ����Ʒ�༭
-	Intent intent=new Intent(this, GoodsEdit.class);
-	startActivity(intent);
-}
+	private void clickPlusBtn() {
+		// TODO Auto-generated method stub
+		// ������Ʒ����Ʒ�༭
+		Intent intent = new Intent(this, GoodsEdit.class);
+		startActivity(intent);
+	}
 
-/**
+	/**
  * 
  */
-private void clickHomeBtn() {
-	// TODO Auto-generated method stub
-	if(!layout_home.isSelected()){
-		//ʵ����Fragmentҳ��  
-    fragment_home = new Fragment_home();  
-    //�õ�Fragment���������  
-    FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();
-    fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right);
-    //�滻��ǰ��ҳ��  
-    fragmentTransaction.replace(R.id.frame_content, fragment_home);  
-    //��������ύ  
-    fragmentTransaction.commit();  
-    
-    layout_home.setSelected(true);
-    image_home.setSelected(true); 
-   
-    layout_my.setSelected(false);  
-    image_my.setSelected(false);  
+	private void clickHomeBtn() {
+		// TODO Auto-generated method stub
+		if (!layout_home.isSelected()) {
+			// ʵ����Fragmentҳ��
+			fragment_home = new Fragment_home();
+			// �õ�Fragment���������
+			FragmentTransaction fragmentTransaction = this
+					.getSupportFragmentManager().beginTransaction();
+			fragmentTransaction.setCustomAnimations(R.animator.slide_in_right,
+					R.animator.slide_out_left);
+			// �滻��ǰ��ҳ��
+			fragmentTransaction.replace(R.id.frame_content, fragment_home);
+			// ��������ύ
+			fragmentTransaction.commit();
+
+			layout_home.setSelected(true);
+			image_home.setSelected(true);
+
+			layout_my.setSelected(false);
+			image_my.setSelected(false);
+		}
+
 	}
-      
 
-}
+	private void clickMyBtn() {
+		// TODO Auto-generated method stub
+		if (!layout_my.isSelected()) {// ʵ����Fragmentҳ��
+			fragment_my = new Fragment_my();
+			// �õ�Fragment���������
+			FragmentTransaction fragmentTransaction = this
+					.getSupportFragmentManager().beginTransaction();
+			fragmentTransaction.setCustomAnimations(R.animator.slide_in_left,
+					R.animator.slide_out_right);
+			// �滻��ǰ��ҳ��
+			fragmentTransaction.replace(R.id.frame_content, fragment_my);
+			// ��������ύ
+			fragmentTransaction.commit();
+			layout_my.setSelected(true);
+			image_my.setSelected(true);
+			layout_home.setSelected(false);
+			image_home.setSelected(false);
+		}
 
-
-private void clickMyBtn() {
-	// TODO Auto-generated method stub
-	if(!layout_my.isSelected())
-	{//ʵ����Fragmentҳ��  
-    fragment_my = new Fragment_my();  
-    //�õ�Fragment���������  
-    FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();  
-    fragmentTransaction.setCustomAnimations(R.animator.slide_in_left, R.animator.slide_out_right);
-    //�滻��ǰ��ҳ��
-    fragmentTransaction.replace(R.id.frame_content, fragment_my);  
-    //��������ύ
-    fragmentTransaction.commit();  
-    layout_my.setSelected(true);  
-    image_my.setSelected(true);
-    layout_home.setSelected(false);
-    image_home.setSelected(false);  
 	}
-    
-
-}
 
 }
